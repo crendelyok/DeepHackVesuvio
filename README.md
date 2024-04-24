9a3e7dd51e740377ea05f92dccd3737a5e9e8e4b
crendelyok <nikita.pankov.v@gmail.com>
2024-04-24 13:32:41 +0300

    feat/initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Prerequisites
+
+1) python3.10
+
+
+# Installation
+
+## venv
+```
+python3.12 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## docker: todo
---
+++ tmp/check_api.py
@@ -0,0 +1,19 @@
+"""Пример работы с чатом через gigachain"""
+from langchain.schema import HumanMessage, SystemMessage
+from langchain.chat_models.gigachat import GigaChat
+
+# Авторизация в сервисе GigaChat
+chat = GigaChat(credentials="", verify_ssl_certs=False)
+
+messages = [
+    SystemMessage(
+        content="Ты эмпатичный бот-психолог, который помогает пользователю решить его проблемы."
+    )
+]
+
+while(True):
+    user_input = input("User: ")
+    messages.append(HumanMessage(content=user_input))
+    res = chat(messages)
+    messages.append(res)
+    print("Bot: ", res.content)
