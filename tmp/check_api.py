9b69f345a3df52a878df8564298c5cf3468f2208
crendelyok <nikita.pankov.v@gmail.com>
2024-04-24 21:33:06 +0300

    feat/env creds

--- tmp/check_api.py
+++ tmp/check_api.py
@@ -1,14 +1,21 @@
 """Пример работы с чатом через gigachain"""
 
+import os
+
 from langchain.chat_models.gigachat import GigaChat
 from langchain.schema import HumanMessage, SystemMessage
 
 # Авторизация в сервисе GigaChat
-chat = GigaChat(credentials="", verify_ssl_certs=False)
+chat = GigaChat(
+    credentials=os.environ["GIGACHAD_VIP_CREDS"],
+    verify_ssl_certs=False,
+    scope="GIGACHAT_API_CORP",
+    model="GigaChat-Pro",
+)
 
 messages = [
     SystemMessage(
-        content="Ты эмпатичный бот-психолог, который помогает пользователю решить его проблемы."
+        content="Ты крутой бот Итачи из Наруто, который помогает пользователю решить его проблемы."
     )
 ]
 
