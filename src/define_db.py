d43566af34269f5402b830047dc3ee8a2c727a22
rogovrt <artemrogov@list.ru>
2024-04-25 18:58:45 +0300

    Without downloading pdf

--- src/define_db.py
+++ src/define_db.py
@@ -1,28 +1,11 @@
 import concurrent.futures
 import os
-import time
 
-import requests
 from langchain.chains.summarize import load_summarize_chain
 from langchain.chat_models.gigachat import GigaChat
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
-
-
-def download_pdf(url):
-    response = requests.get(url, stream=True)
-    filename = "../articles/" + str(time.time()) + ".pdf"
-    with open(filename, "wb") as fd:
-        for chunk in response.iter_content(chunk_size=1024):
-            fd.write(chunk)
-    return filename
-
-
-def is_valid_url(url):
-    response = requests.get(url)
-    return response.status_code == 200
-
 
 model = GigaChat(
     credentials=os.environ["GIGACHAD_VIP_CREDS"],
@@ -53,12 +36,7 @@ refine_prompt = PromptTemplate.from_template(refine_template)
 
 
 def get_article_summary(path_to_pdf):
-    if is_valid_url(path_to_pdf):
-        path_to_pdf = download_pdf(path_to_pdf)
-    if os.path.exists(path_to_pdf):
-        loader = PyPDFLoader(path_to_pdf)
-    else:
-        raise RuntimeError(f"{path_to_pdf} is incorrect path to pdf file")
+    loader = PyPDFLoader(path_to_pdf)
 
     docs = loader.load()
 
@@ -83,5 +61,4 @@ def get_article_summary(path_to_pdf):
 def get_summary_of_all_articles(paths):
     with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
         results = list(executor.map(get_article_summary, paths))
-    # [print(res) for res in results]
     return results
